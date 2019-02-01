ae0586f9a806cc807ed50375d8af0e33f2905cf6
Islam <islam.sayed8@gmail.com>
2019-02-01 10:44:50 +0200

    update

--- blog-template/js/main.js
+++ blog-template/js/main.js
@@ -1,1 +1,16 @@
-var x = 1;
+window.onload = function() {
+  var toggleNav = function() {
+    var toggler = document.querySelector(".navbar-toggler");
+    var heroImg = document.querySelector(".hero-img");
+    var navbar = document.querySelector(".navbar-collapse")
+    toggler.addEventListener("click", function() {
+        if(!navbar.classList.contains('show')) {
+            heroImg.style.marginTop = "0";
+        } else {
+            heroImg.style.marginTop = "-110px";
+            heroImg.style.transition = ".6s"
+        }
+    });
+  };
+  toggleNav()
+};
--- blog-template/js/minjs/main.js
+++ blog-template/js/minjs/main.js
@@ -1,1 +1,1 @@
-var x=1;
+window.onload=function(){var e,n,o;e=document.querySelector(".navbar-toggler"),n=document.querySelector(".hero-img"),o=document.querySelector(".navbar-collapse"),e.addEventListener("click",function(){o.classList.contains("show")?(n.style.marginTop="-110px",n.style.transition=".6s"):n.style.marginTop="0"})};
